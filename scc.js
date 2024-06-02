81927bb4f13ada89ed5e452eceabae4841077b87
sh20022002 <shmuel.tor@gmail.com>
2024-06-02 18:22:52 +0300

    --

--- scc.js
+++ scc.js
@@ -30,8 +30,10 @@ function main() {
                 tempArray.push(temp[1]);
             }
         }
+        // console.log(graph['1']);
         var sccs = kosarajus(graph);
-        console.log(sccs);
+        sccs = sccs.filter(scc => scc.length > 31);
+        console.log(sccs[0].length), console.log(sccs[1].length), console.log(sccs[2].length), console.log(sccs[3].length);
     });
 }
 
@@ -60,3 +62,68 @@ class Stack {
         return this.items[this.items.length - 1];
     }
 }
+function kosarajus(graph) {
+    let stack = new Stack();
+    let visited = {};
+    let sccs = {};
+    let reversedGraph = reverseGraphf(graph);
+    function DFS(graph, vertex, visited, scc = null) {
+        visited[vertex] = true;
+        
+        for (let neighbor in graph[vertex]) {
+            if (!visited[neighbor]) {
+                scc.push(neighbor);
+                DFS(graph, neighbor, visited, scc);
+            }
+        }
+        
+        if (scc !== null) {
+            sccs[vertex] = scc;
+        }
+        
+        }
+    }
+    function reverseGraphf(graph) {
+        const reversedGraph = {};
+    
+        // Initialize the reversed graph with empty arrays for each vertex
+        for (let vertex in graph) {
+            if (graph.hasOwnProperty(vertex)) {
+                reversedGraph[vertex] = [];
+            }
+        }
+    
+        // Iterate over each vertex and its edges
+        for (let vertex in graph) {
+            if (graph.hasOwnProperty(vertex)) {
+                graph[vertex].forEach(edge => {
+                    // If the edge vertex is not yet in the reversedGraph, initialize it
+                    if (!reversedGraph.hasOwnProperty(edge)) {
+                        reversedGraph[edge] = [];
+                    }
+                    // Add the reverse edge
+                    reversedGraph[edge].push(vertex);
+                });
+            }
+        }
+    
+        return reversedGraph;
+    }
+    for (let vertex in reversedGraph) {
+        if (!visited[vertex]) {
+            DFS(reversedGraph, vertex, visited);
+        }
+    }
+    visited = {};
+    while (!stack.isEmpty()) {
+        let vertex = stack.pop();
+        if (!visited[vertex]) {
+            let scc = [];
+            DFS(graph, vertex, visited, scc);
+            sccs.push(scc);
+        }
+    }
+    return sccs;
+    
+}
+main();
